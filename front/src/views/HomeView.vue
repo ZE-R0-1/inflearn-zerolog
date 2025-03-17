033a5f80742e2aeaa25156517abd994a5c518950
ZERO <jijiij77@naver.com>
2025-03-17 15:51:30 +0900

    글 내용 화면 만들기

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import axios from "axios";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
 
+const router = useRouter()
 const posts = ref([]);
 
 axios.get("/api/posts?page=1&size=5").then((response) => {
@@ -9,13 +11,16 @@ axios.get("/api/posts?page=1&size=5").then((response) => {
     posts.value.push(r);
   });
 })
+
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.id">
       <div>
-        {{ post.title }}
+        <router-link :to="{ name: 'read', params: { postId: post.id } }">{{
+            post.title
+        }}</router-link>
       </div>
 
       <div>
